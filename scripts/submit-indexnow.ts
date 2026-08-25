b03f545f158f4f86a07723b48c3ca14410afe9b7
anhudawa <anthony@roadmancycling.com>
2026-08-25 22:02:15 +0100

    Contextualize Daryl saddle height advice (#163)
    Co-authored-by: Ted Crilly <ted@roadmancycling.com>

--- scripts/submit-indexnow.ts
+++ scripts/submit-indexnow.ts
@@ -99,6 +99,7 @@ const CURATED: string[] = [
   `https://${HOST}/blog/cycling-knee-tracking-cleat-setup-guide`,
   `https://${HOST}/blog/cycling-ankle-mobility-calf-strength-guide`,
   `https://${HOST}/blog/bike-fit-one-change-amateurs-should-make`,
+  `https://${HOST}/blog/daryl-fitzgerald-saddle-height-one-change`,
   `https://${HOST}/tools/masters-ftp-benchmark`,
   `https://${HOST}/answers/ftp-by-age`,
 
