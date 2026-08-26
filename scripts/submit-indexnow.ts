39596ef6bcce8deac606bd4011e299e9317b2d1e
anhudawa <anthony@roadmancycling.com>
2026-08-26 05:38:01 +0100

    seo: strengthen in-ride nutrition owner (#193)
    Co-authored-by: Ted Crilly <ted@roadmancycling.com>

--- scripts/submit-indexnow.ts
+++ scripts/submit-indexnow.ts
@@ -144,6 +144,7 @@ const CURATED: string[] = [
   `https://${HOST}/blog/cycling-knee-tracking-cleat-setup-guide`,
   `https://${HOST}/blog/cycling-ankle-mobility-calf-strength-guide`,
   `https://${HOST}/blog/cycling-energy-gels-guide`,
+  `https://${HOST}/blog/cycling-in-ride-nutrition-guide`,
   `https://${HOST}/blog/bike-fit-one-change-amateurs-should-make`,
   `https://${HOST}/blog/daryl-fitzgerald-saddle-height-one-change`,
   `https://${HOST}/podcast/ep-1-pro-bike-fitter-reveals-the-1-change-amateurs-should-make`,
